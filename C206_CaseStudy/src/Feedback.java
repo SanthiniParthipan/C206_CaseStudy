419d1f2baceb6d294fe69073dfcce4b53d9decaf
SanthiniParthipan <19028654@myrp.edu.sg>
2020-08-18 22:30:58 +0800

    santhini added feedback class

--- C206_CaseStudy/src/Feedback.java
+++ C206_CaseStudy/src/Feedback.java
@@ -1,4 +1,30 @@
 
 public class Feedback {
+	
+	private String Customer ;
+	private String feedback;
+	private String response;
+	public String getCustomer() {
+		return Customer;
+	}
+	public void setCustomer(String customer) {
+		Customer = customer;
+	}
+	public String getFeedback() {
+		return feedback;
+	}
+	public void setFeedback(String feedback) {
+		this.feedback = feedback;
+	}
+	public String getResponse() {
+		return response;
+	}
+	public void setResponse(String response) {
+		this.response = response;
+	}
 
+	
+	public String toString[]; 
+		
+	
 }
