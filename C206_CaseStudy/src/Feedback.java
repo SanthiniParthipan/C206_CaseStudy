b7337836954f218515bb02a32167b278a03569a7
SanthiniParthipan <19028654@myrp.edu.sg>
2020-08-19 18:29:09 +0800

    feedback class

--- C206_CaseStudy/src/Feedback.java
+++ C206_CaseStudy/src/Feedback.java
@@ -4,27 +4,39 @@ public class Feedback {
 	private String Customer ;
 	private String feedback;
 	private String response;
-	public String getCustomer() {
-		return Customer;
-	}
-	public void setCustomer(String customer) {
+	
+	public Feedback(String customer, String feedback, String response) {
+		super();
 		Customer = customer;
-	}
-	public String getFeedback() {
-		return feedback;
-	}
-	public void setFeedback(String feedback) {
 		this.feedback = feedback;
-	}
-	public String getResponse() {
-		return response;
-	}
-	public void setResponse(String response) {
 		this.response = response;
 	}
 
+	public String getCustomer() {
+		return Customer;
+	}
+
+	public void setCustomer(String customer) {
+		Customer = customer;
+	}
+
+	public String getFeedback() {
+		return feedback;
+	}
+
+	public void setFeedback(String feedback) {
+		this.feedback = feedback;
+	}
+
+	public String getResponse() {
+		return response;
+	}
+
+	public void setResponse(String response) {
+		this.response = response;
+	}
 	
 	public String toString[]; 
-		
+	
 	
 }
