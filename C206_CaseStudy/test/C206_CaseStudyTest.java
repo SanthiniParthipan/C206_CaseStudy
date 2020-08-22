a9fcf70ff32926a3e5c34ac6c06cb607c27bf18f
SanthiniParthipan <19028654@myrp.edu.sg>
2020-08-22 12:51:12 +0800

    up

--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -212,6 +212,22 @@ public class C206_CaseStudyTest {
 		
 	}
 	// delete for(all 5)
+	
+	public void deleteBikePartTest() {
+	    //boundary
+	    assertNotNull("Test if there is valid BikePart arraylist to add to", BikePartList);
+	    C206_CaseStudyTest.addBikeParts(BikePartList, bp1);
+	    //normal
+	    Boolean ok = C206_CaseStudyTest.deleteBikeParts(BikePartList,"b001");
+	    assertTrue("Test if an available item is ok to delete?", ok);    
+	    //error condition
+	    ok = C206_CaseStudyTest.deleteBikeParts(BikePartList,"");
+	    assertFalse("Test if the same item is NOT ok to delete ?", ok);  
+	    //error
+	    ok = C206_CaseStudyTest.deleteBikeParts(BikePartList,"b1111"); 
+	    assertFalse("Test if the same item is NOT ok to delete ?", ok);  
+	    
+	  }
 
 	public void deleteCustomerTest() {
 	    //normal
@@ -240,6 +256,17 @@ public class C206_CaseStudyTest {
 	  }
 
 	
+	private static Boolean deleteBikeParts(ArrayList<BikeParts> bikePartList2, String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static void addBikeParts(ArrayList<BikeParts> bikePartList2, BikeParts bp12) {
+		// TODO Auto-generated method stub
+		
+	}
+
+ 
 	@After
 	public void tearDown() throws Exception {
 		cu1=null;
